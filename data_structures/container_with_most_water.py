ec599d95122a90a8cf0b35983ec7d7fdf57a8425
vishal.g <vishal.g@fireeye.com>
2023-01-04 01:38:21 +0530

    container water problem

--- data_structures/container_with_most_water.py
+++ data_structures/container_with_most_water.py
@@ -24,9 +24,37 @@ n == height.length
 
 """
 
+# method1: worst approach O(n2)
+def max_area_for_water(input_height):
+    input_height_length = len(input_height)
+    area = 0
+    for i in range(input_height_length):
+        for j in range(i+1, input_height_length):
+            area = max(area, min(input_height[i], input_height[j]) * (j - i))
+    return area
+
+# method2: best approach O(n)
+def max_area_for_water1(input_height):
+    l = 0
+    r = len(input_height)-1
+    area = 0
+
+    while l < r:
+        area = max(area, min(input_height[l], input_height[r]) * (r - l))
+
+        if input_height[l] < input_height[r]:
+            l += 1
+        else:
+            r -= 1
+    return area
 
 
 
+input_height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+print(max_area_for_water(input_height))
+
+print(max_area_for_water1(input_height))
 
 
 
